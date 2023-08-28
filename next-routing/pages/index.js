c15697f1585a1e8f43c2cca3be4b451b7fa1e321
shreeyansh-29 <shreeyansh.rawat@gmail.com>
2023-08-28 19:59:40 +0530

    1)navigating programmatically 2)custom 404 page

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 const index = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/product");
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href="/blog">
-      Blog
-        {/* <a>Blog</a> */}
-      </Link>
-      <Link href="/product">
-        Product
-      </Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Product</Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 };
