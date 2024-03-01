c993e71363467220b95994ec79062991448e9492
shreeyansh-29 <shreeyansh.rawat@gmail.com>
2024-03-01 11:39:23 +0530

    handling DELETE request

--- router-handlers-demo/src/app/comments/[id]/route.ts
+++ router-handlers-demo/src/app/comments/[id]/route.ts
@@ -13,3 +13,11 @@ export async function PATCH(request: Request, {params}: {params: {id:string}}) {
 
   return Response.json(comments[index]);
 }
+
+export async function DELETE(_request: Request, {params}: {params: {id:string}}) {
+  const index = comments.findIndex((comment) => comment.id === parseInt(params.id))
+  const deleteComment = comments[index];
+  comments.splice(index,1);
+  
+  return Response.json(deleteComment);
+}
