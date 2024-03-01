ff8e6af0222adb2f1a9abb4e03dfdc2e03e44d35
shreeyansh-29 <shreeyansh.rawat@gmail.com>
2024-03-01 11:51:50 +0530

    redirects in route handlers

---
+++ router-handlers-demo/src/app/profile/api/route.ts
@@ -0,0 +1,16 @@
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const reqHeaders = new Headers(request.headers);
+    const headerList = headers();
+    console.log(reqHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+    
+    return new Response('<h1>Profile API Data</h1>', {
+        headers: {
+          "Content-Type": "text/html"
+        }
+    })
+}
+
